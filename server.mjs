319083d95552fe3a36c82b2aa85e3f5600404573
Umairkhan1929 <umairkhanit1@gmial.com>
2024-10-04 14:29:41 +0500

    import express from express

---
+++ server.mjs
@@ -0,0 +1,34 @@
+import express from 'express'
+
+// const PORT = 3000
+// const app = express()
+
+// // json middleware
+// app.use(express.json())
+
+// app.get('/api/items', (req, res) => {
+//     res.status(200).json({ message: 'GET request - Fetching all items' });
+// })
+
+// app.post('/api/items', (req, res) => {
+//     const newItem = req.body;
+//     newItem.id = 1;
+//     res.status(201).json({ message: 'New item created!', data: newItem })
+// })
+
+// // update item
+// app.put('/api/items/:id', (req, res) => {
+//     const itemId = req.params.id
+//     const updatedItem = req.body
+//     res.status(200).json({ message: `PUT request - Updating item ${itemId}`, data: updatedItem })
+// })
+
+// // delete item
+// app.delete('/api/items/:id', (req, res) => {
+//     const itemId = req.params.id
+//     res.status(200).json({ message: `DELETE request - Deleting item ${itemId}` })
+// })
+
+// app.listen(PORT, () => {
+//     console.log(`Server is listening here: http://localhost:${PORT}`)
+// })
